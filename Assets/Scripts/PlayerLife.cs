549e719c9107e1837c28947300d025fe59470c18
Aleksandra Pieńkowska <aleksandra.pienkowska@tensquaregames.com>
2022-05-14 18:20:45 +0200

    Blink effect

--- Assets/Scripts/PlayerLife.cs
+++ Assets/Scripts/PlayerLife.cs
@@ -7,73 +7,63 @@ public class PlayerLife : MonoBehaviour
 {
 	private Animator anim;
 	private Rigidbody2D rb;
-	[SerializeField] private int LifeLevel=100;
+	[SerializeField] private int LifeLevel = 100;
 	[SerializeField] private Text LifeText;
 	SpriteRenderer PlayerSprite;
- public float spriteBlinkingTimer = 0.0f;
- public float spriteBlinkingMiniDuration = 0.1f;
- public float spriteBlinkingTotalTimer = 0.0f;
- public float spriteBlinkingTotalDuration = 1.0f;
- public bool startBlinking = false;
- private Color baseColor;
-    // Start is called before the first frame update
-    void Start()
-    {
-        anim=GetComponent<Animator>();
-		rb=GetComponent<Rigidbody2D>();
-		PlayerSprite=GetComponent<SpriteRenderer>();
-		baseColor=PlayerSprite.color;
-    }
-	
+	public bool startFading = false;
+	private Color baseColor;
+	[SerializeField] private Color blinkColor1;
+	[SerializeField] private Color blinkColor2;
+	[SerializeField] private float blinkTime=0.1f;
+	private float blinkTimer=0.0f;
+	// Start is called before the first frame update
+	void Start()
+	{
+		anim = GetComponent<Animator>();
+		rb = GetComponent<Rigidbody2D>();
+		PlayerSprite = GetComponent<SpriteRenderer>();
+		baseColor = PlayerSprite.color;
+	}
+
 	private void OnCollisionEnter2D(Collision2D collision)
 	{
-		if (collision.gameObject.CompareTag("Trap") && LifeLevel <=5 )
+		if (collision.gameObject.CompareTag("Trap") && LifeLevel <= 5)
 		{
 			Die();
 		}
-		else if (collision.gameObject.CompareTag("Trap")){
-			LifeLevel=LifeLevel-5;
-			LifeText.text="Life: "+LifeLevel;
-			SpriteBlinkingEffect();
+		else if (collision.gameObject.CompareTag("Trap"))
+		{
+			LifeLevel = LifeLevel - 5;
+			LifeText.text = "Life: " + LifeLevel;
+			startFading = true;
+			PlayerSprite.color=blinkColor1;
 		}
 	}
 	
+	private void Update()
+	{
+		
+		if (startFading && blinkTimer<=blinkTime )
+		{
+			blinkTimer=blinkTimer+Time.deltaTime;
+			PlayerSprite.color=blinkColor1;
+		}
+		else{
+			blinkTimer=0.0f;
+			PlayerSprite.color=baseColor;
+			startFading=false;
+		}
+	}
+
 	private void Die()
 	{
-		rb.bodyType=RigidbodyType2D.Static;
+		rb.bodyType = RigidbodyType2D.Static;
 		anim.SetTrigger("death");
 	}
-	
+
 	private void RestartLevel()
 	{
 		SceneManager.LoadScene(SceneManager.GetActiveScene().name);
 	}
-	
-	private void SpriteBlinkingEffect()
-      {
-        spriteBlinkingTotalTimer += Time.deltaTime;
-        if(spriteBlinkingTotalTimer >= spriteBlinkingTotalDuration)
-        {
-              startBlinking = false;
-             spriteBlinkingTotalTimer = 0.0f;
-             PlayerSprite.enabled = true;   
-			 
-             return;
-          }
-     
-     spriteBlinkingTimer += Time.deltaTime;
-     if(spriteBlinkingTimer >= spriteBlinkingMiniDuration)
-     {
-         spriteBlinkingTimer = 0.0f;
-         if (PlayerSprite.enabled == true) {
-             PlayerSprite.enabled = false;  //make changes
-			 PlayerSprite.color = new Color (1, 0, 0, 1);
-         } else {
-             PlayerSprite.enabled = true;   //make changes
-			 PlayerSprite.color = new Color (1, 0, 0, 0);
-			 
-         }
-     }
-	  PlayerSprite.color = baseColor;
-	}
+
 }
