d883e48777203b0afec4936ab958e005b3caaead
Aleksandra Pieńkowska <aleksandra.pienkowska@tensquaregames.com>
2022-05-15 09:47:09 +0200

    Fixes

--- Assets/SceneTransition.cs
+++ Assets/SceneTransition.cs
@@ -6,15 +6,12 @@ using UnityEngine.SceneManagement;
 public class SceneTransition : MonoBehaviour
 {
 	public string sceneToLoad;
-	public Vector2 playerPosition;
-	public VectorValue playerStorage;
 	
 	public void onTriggerEnter2D(Collider2D other)
 	{
 		Debug.Log("New sceneLoad");
 		if(other.CompareTag("Player") && ! other.isTrigger)
 		{
-			playerStorage.initialValue=playerPosition;
 			SceneManager.LoadScene(sceneToLoad);
 		}
 	}
